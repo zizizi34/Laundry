c429740a057277c5eb014d9f50cb3bbae52dae91
LENOVO <narendrafahrezi33@gmail.com>
2025-01-31 10:56:45 +0700

    Menambahkan Tambah Pelanggan dan Data pelanggan

---
+++ app/src/main/java/com/example/laundry/Pelanggan/TambahPelanggan.kt
@@ -0,0 +1,21 @@
+package com.example.laundry.Pelanggan
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.laundry.R
+
+class TambahPelanggan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tambah_pelanggan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
