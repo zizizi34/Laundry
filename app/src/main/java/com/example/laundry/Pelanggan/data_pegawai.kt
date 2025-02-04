5bdfa03a25faaa4c8a060707207c023e1809a0e6
LENOVO <narendrafahrezi33@gmail.com>
2025-02-04 20:30:49 +0700

    Menambahkan data pegawai,tambah pegawai,gambar,dan merapikan layoutnya

---
+++ app/src/main/java/com/example/laundry/Pelanggan/data_pegawai.kt
@@ -0,0 +1,21 @@
+package com.example.laundry.Pelanggan
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.laundry.R
+
+class data_pegawai : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_data_pegawai)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
