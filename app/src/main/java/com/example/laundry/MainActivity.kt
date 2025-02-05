4cc1e32fcf9df530bc024cb11d10f64aefa95134
LENOVO <narendrafahrezi33@gmail.com>
2025-02-05 13:29:28 +0700

    Menambahkan Pelanggan

--- app/src/main/java/com/example/laundry/MainActivity.kt
+++ app/src/main/java/com/example/laundry/MainActivity.kt
@@ -5,12 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.laundry.Pelanggan.DataPelanggan
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+//        redirect to pelanggan
+        val appDataPelanggan = findViewById<ConstraintLayout>(R.id.clPelanggan)
+        appDataPelanggan.setOnClickListener{
+            val intent = Intent(this, DataPelanggan::class.java)
+            startActivity(intent)
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/laundry/Pelanggan/DataPelanggan.kt
+++ app/src/main/java/com/example/laundry/Pelanggan/DataPelanggan.kt
@@ -1,10 +1,14 @@
 package com.example.laundry.Pelanggan
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.example.laundry.R
 
 class DataPelanggan : AppCompatActivity() {
@@ -12,6 +16,13 @@ class DataPelanggan : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_data_pelanggan)
+
+        val tambahPelanggan = findViewById<FloatingActionButton>(R.id.fabDATA_PENGGUNA_TAMBAH)
+        tambahPelanggan.setOnClickListener {
+            val intent = Intent(this, TambahPelanggan::class.java)
+            startActivity(intent)
+            }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
